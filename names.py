ca15d2358fd083fcc99b56bb0f0a085441955e2d
Erik M. Bray <embray@stsci.edu>
2012-10-26 14:49:19 -0400

    Cleaned up unmerged conflict mess.

--- names.py
+++ names.py
@@ -1,24 +1,17 @@
 def erik():
     print "Hi, I'm Erik."
 
-<<<<<<< HEAD
-der paul():
+def paul():
     print "Hi, back."
-=======
+
 def john():
     print "I'm John."
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 def Shea():
-	print "Hello. Shea here."
+    print "Hello. Shea here."
 
-=======
 def J():
     print "Hi, my name is J."
->>>>>>> 6d6467ad3ecb6179b3771aa32dcc4a78a4b54768
-=======
+
 def Devin():
     print "Hello, world!"
->>>>>>> ed392a3755ef2f368722feb00240296db08bf99c
->>>>>>> baffd069b8bccf5cd5be86ba9c06f3168d1f87aa
