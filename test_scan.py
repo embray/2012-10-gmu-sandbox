14ce83a93d5d5d1a503f7e9afc80ef8dcfcf6e40
Erik M. Bray <embray@stsci.edu>
2012-10-26 15:58:28 -0400

    test_scan.py

--- test_scan.py
+++ test_scan.py
@@ -4,18 +4,18 @@ def scan(lst):
     if len(lst) == 0:
         return 0
 
-scan([])                 == 0
-scan([1, 2, 3, 4])       == 10
-scan([2, 3, 4, 5])       == 14
-scan([1, 1, 1, 1])       == 1
-scan([3, 2, 5, 4])       == 0
-scan([4, 3, 2, 1])       == 0
-scan([3, 4, 5, 1])       == 0
-scan([1, 2, 3, 5, 4])    == 0
-scan([1, 2, 3, 3])       == 6
-scan([1, 2, 4, 4])       == 7
-scan([1, 2, 4, 4, 5])    == 12
-scan([-3, -2, -1])       == -6
-scan([-1, -2, -3])       == -3
-scan([-1, -2, -3, -2, -1]) == -6
-scan([3, 2, 1, 2, 3])      == 6
+assert scan([])                 == 0
+assert scan([1, 2, 3, 4])       == 10
+assert scan([2, 3, 4, 5])       == 14
+assert scan([1, 1, 1, 1])       == 1
+assert scan([3, 2, 5, 4])       == 0
+assert scan([4, 3, 2, 1])       == 0
+assert scan([3, 4, 5, 1])       == 0
+assert scan([1, 2, 3, 5, 4])    == 0
+assert scan([1, 2, 3, 3])       == 6
+assert scan([1, 2, 4, 4])       == 7
+assert scan([1, 2, 4, 4, 5])    == 12
+assert scan([-3, -2, -1])       == -6
+assert scan([-1, -2, -3])       == -3
+assert scan([-1, -2, -3, -2, -1]) == -6
+assert scan([3, 2, 1, 2, 3])      == 6
